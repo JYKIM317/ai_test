82288287afe4163318e6b0dd10eb6cddee766db1
Jinyoung Kim <jinyoungkim@Jinyoungs-MacBook-Air.local>
2024-03-31 21:30:27 +0900

    add image crop logic

--- lib/data/repository/image_repository.dart
+++ lib/data/repository/image_repository.dart
@@ -1,15 +1,45 @@
 import 'package:ai_test/data/source/local/camera_local.dart';
 import 'package:ai_test/data/source/local/gallery_local.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 class ImageRepository {
   Future<String?> getCameraImageFilePath() async {
+    String? imagePath;
     XFile? image = await LocalCamera().getImgae();
-    return image?.path;
+    if (image != null) {
+      imagePath = await imageCropper(imagePath: image.path);
+    }
+    return imagePath;
   }
 
   Future<String?> getGarlleryImageFilePath() async {
+    String? imagePath;
     XFile? image = await LocalGallery().getImgae();
-    return image?.path;
+    if (image != null) {
+      imagePath = await imageCropper(imagePath: image.path);
+    }
+    return imagePath;
   }
+}
+
+//
+
+Future<String?> imageCropper({required String imagePath}) async {
+  String? path;
+  CroppedFile? croppedImage = await ImageCropper().cropImage(
+    sourcePath: imagePath,
+    uiSettings: [
+      AndroidUiSettings(
+        initAspectRatio: CropAspectRatioPreset.original,
+        lockAspectRatio: false,
+      )
+    ],
+  );
+
+  if (croppedImage != null) {
+    path = croppedImage.path;
+  }
+
+  return path;
 }
