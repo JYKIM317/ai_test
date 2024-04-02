689bdd1bedf80eec6f858c1c6a6dde1f1cc5189e
Jinyoung Kim <jinyoungkim@Jinyoungs-MacBook-Air.local>
2024-04-02 12:04:00 +0900

    generative_ai remote extends api super class

--- lib/data/source/remote/generative_ai_remote.dart
+++ lib/data/source/remote/generative_ai_remote.dart
@@ -1,11 +1,11 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:ai_test/properties.dart';
 
-class RemoteGenerativeAI {
+class RemoteGenerativeAI extends APIkeys {
   Future<GenerateContentResponse> promptResponse(
       {required String prompt}) async {
     final model =
-        GenerativeModel(model: 'gemini-pro', apiKey: APIkeys().geminiApiKey);
+        GenerativeModel(model: 'gemini-pro', apiKey: super.geminiApiKey);
 
     final content = [Content.text(prompt)];
     return await model.generateContent(content);
