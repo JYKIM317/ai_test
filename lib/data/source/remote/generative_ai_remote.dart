b48923234710a02ee5e72462b9fbca3591752bd3
Jinyoung Kim <jinyoungkim@Jinyoungs-MacBook-Air.local>
2024-04-03 18:05:19 +0900

    Refectory GenerativeModel's model name to final String

--- lib/data/source/remote/generative_ai_remote.dart
+++ lib/data/source/remote/generative_ai_remote.dart
@@ -2,10 +2,12 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:ai_test/properties.dart';
 
 class RemoteGenerativeAI extends APIkeys {
+  final String _modelName = 'gemini-pro';
+
   Future<GenerateContentResponse> promptResponse(
       {required String prompt}) async {
     final model =
-        GenerativeModel(model: 'gemini-pro', apiKey: super.geminiApiKey);
+        GenerativeModel(model: _modelName, apiKey: super.geminiApiKey);
 
     final content = [Content.text(prompt)];
     return await model.generateContent(content);
